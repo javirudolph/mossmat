23aee300de62c057fb143bf7ff96f313bc3d31f6
javirudolph <javiera.av@ufl.edu>
2019-05-28 21:15:38 -0400

    data for voc only and zeroed if negative

--- R/voc_eda.R
+++ R/voc_eda.R
@@ -17,6 +17,17 @@ voc_data <- rawdata[,c(1,2,4,5, 20:109)] %>%
 
 # Shorten voc names
 oldnames <- names(voc_data)
-names(voc_data)[4:93] <- stringr::str_trunc(oldnames[4:93], width = 6, side = "right", ellipsis = "_") 
+names(voc_data)[4:93] <- stringr::str_trunc(oldnames[4:93], width = 6, side = "right", ellipsis = "") 
 
 # Any negative values are zero
+# Keep the maximum value for each sample
+makezero <- function(x) ifelse(x < 0, 0, x)
+takemax <- function(x) max(x)
+
+voc_data %>% 
+  mutate_if(is.numeric, makezero) -> max_voc
+  
+  
+
+
+
