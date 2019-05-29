9bf747fed8e61aefb9539114682fee695d7f8e7a
javirudolph <javiera.av@ufl.edu>
2019-05-29 09:29:09 -0400

    function to filter out data

--- R/voc_eda.R
+++ R/voc_eda.R
@@ -10,7 +10,7 @@ rawdata <- read.csv("rawdata/LK_master.csv")
 #' Also, remove the NAs, there's a lot at the end
 #' Remove (2) notation from sample ids
 #' Remove May 30 samples
-voc_data <- rawdata[,c(1,2,4,5, 20:109)] %>% 
+voc_raw <- rawdata[,c(1,2,4,5, 20:109)] %>% 
   filter(Date.PTR != "30-May") %>% 
   select(-Date.PTR) %>% 
   rename(famid = `Fam..`,
@@ -20,14 +20,14 @@ voc_data <- rawdata[,c(1,2,4,5, 20:109)] %>%
   mutate(sampid = str_replace_all(sampid, "\\(.*\\)", ""))
 
 #' Shorten voc names
-oldnames <- names(voc_data)
-names(voc_data)[4:93] <- stringr::str_trunc(oldnames[4:93], width = 6, side = "right", ellipsis = "") 
+oldnames <- names(voc_raw)
+names(voc_raw)[4:93] <- stringr::str_trunc(oldnames[4:93], width = 6, side = "right", ellipsis = "") 
 
-str(voc_data)
+str(voc_raw)
 #' m111.0 is a factor
-voc_data$m111.0
+voc_raw$m111.0
 #' The notation is different, using a capital E
-voc_data$m111.0 <- as.numeric(str_replace(voc_data$m111.0, "E", "e"))
+voc_raw$m111.0 <- as.numeric(str_replace(voc_raw$m111.0, "E", "e"))
 
 
 
@@ -36,9 +36,9 @@ voc_data$m111.0 <- as.numeric(str_replace(voc_data$m111.0, "E", "e"))
 #' Keep the maximum value for each sample
 makezero <- function(x) ifelse(x < 0, 0, x)
 
-voc_data %>% 
+voc_raw %>% 
   mutate_if(is.numeric, makezero) %>% 
-  arrange(sampid, .by_group = TRUE)-> voc_noneg
+  arrange(sampid, .by_group = TRUE)-> voc_data
   
 #' Questions here regarding the next steps
 #' should we first select the max values for specific voc?
@@ -46,14 +46,25 @@ voc_data %>%
 #' IMO we should select the max for each sample, so each sample has only one value and then we can select 10 or 20% representation. If not, some samples would get extra?
 #' 
 
-voc_noneg %>% 
-  select(-famid, -sampid, -ssex) %>% 
-  mutate_if(is.numeric, function(x) ifelse(x > 0, 1, 0)) %>% 
-  colSums() %>% 
-  data.frame() %>% 
-  rownames_to_column() %>%
-  setNames(., c("voc", "count")) %>% 
-  mutate(prcnt = count/548) -> voc_obs
+
+# This function works to remove the volatiles that are not found in a given percentage threshold for observations
+voc_filter <- function(voc_data, threshold = NULL){
+  voc_data %>% 
+    select(-famid, -sampid, -ssex) %>% 
+    mutate_if(is.numeric, function(x) ifelse(x > 0, 1, 0)) %>% 
+    colSums() %>% 
+    data.frame() %>% 
+    rownames_to_column() %>%
+    setNames(., c("voc", "count")) %>% 
+    mutate(prcnt = count/nrow(voc_data)) %>% 
+    filter(prcnt >= threshold) -> voc_filter
+  
+  voc_data %>% 
+    select(famid, sampid, ssex, c(voc_filter$voc))
+  
+}
+
+test <- voc_filter(voc_data, 0.4)
   
   
 
