ae395f412b4c144e531dad63ccc93863979c9c72
javirudolph <javiera.av@ufl.edu>
2019-05-28 21:42:54 -0400

    removed parentheses in sample id notation

--- R/voc_eda.R
+++ R/voc_eda.R
@@ -7,18 +7,21 @@ library(tidyverse)
 rawdata <- read.csv("rawdata/LK_master.csv")
 
 #' Just keep volatile data
+#' Also, remove the NAs, there's a lot at the end
+#' Remove (2) notation from sample ids
 voc_data <- rawdata[,c(1,2,4,5, 20:109)] %>% 
   filter(Date.PTR != "30-May") %>% 
   select(-Date.PTR) %>% 
   rename(famid = `Fam..`,
          sampid = Sample.Name,
          ssex = Sample_Sex) %>% 
-  drop_na()
-
+  drop_na() %>% 
+  mutate(sampid = str_replace_all(sampid, "\\(.*\\)", ""))
 
 #' Shorten voc names
 oldnames <- names(voc_data)
 names(voc_data)[4:93] <- stringr::str_trunc(oldnames[4:93], width = 6, side = "right", ellipsis = "") 
+
 
 #' Any negative values are zero
 #' Keep the maximum value for each sample
@@ -32,7 +35,6 @@ voc_data %>%
 #' should we first select the max values for specific voc?
 #' or should we actually create these 10, 20% cutoffs firs?
 #' IMO we should select the max for each sample, so each sample has only one value and then we can select 10 or 20% representation. If not, some samples would get extra?
-#' 
-#' Just realized, still need to fix some of those '(2)' comments on the sample name
+
 
 
