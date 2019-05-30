e84a8174c88e179fa184d088c684c66cd719df23
javirudolph <javiera.av@ufl.edu>
2019-05-30 13:02:29 -0400

    new rmd, and data analysis

--- R/voc_eda.R
+++ R/voc_eda.R
@@ -1,81 +1,30 @@
+# Exploratory Analysis of VOCs
+# Trying to cluster/group some of the compounds to reduce dimensionality
 
-#' load the necessary libraries
+
+# load the necessary libraries
 library(tidyverse)
 library(vegan)
 library(plotly)
 library(corrplot)
 
-#' Read the csv file
-rawdata <- read.csv("rawdata/LK_master.csv")
-
-#' Just keep volatile data
-#' Also, remove the NAs, there's a lot at the end
-#' Remove (2) notation from sample ids
-#' Remove May 30 samples
-voc_raw <- rawdata[,c(1,2,4,5, 20:109)] %>% 
-  filter(Date.PTR != "30-May") %>% 
-  select(-Date.PTR) %>% 
-  rename(famid = `Fam..`,
-         sampid = Sample.Name,
-         ssex = Sample_Sex) %>% 
-  drop_na() %>% 
-  mutate(sampid = str_replace_all(sampid, "\\(.*\\)", ""))
-
-#' Shorten voc names
-oldnames <- names(voc_raw)
-names(voc_raw)[4:93] <- stringr::str_trunc(oldnames[4:93], width = 6, side = "right", ellipsis = "") 
-
-str(voc_raw)
-#' m111.0 is a factor
-voc_raw$m111.0
-#' The notation is different, using a capital E
-voc_raw$m111.0 <- as.numeric(str_replace(voc_raw$m111.0, "E", "e"))
+# The clean RDS data file should be available locally after you run the master_cleanup.R file.This will take the master csv data file and output the RDS we use here.
+# source("R/master_cleanup.R")
+voc_clean <- readRDS("cleandata/voc_10.RDS")
 
 
+# ANALYSIS ----------------------------------------------------------------
 
+# Data is not in a great format. Very small numbers and many zeroes
+# If we do a log transformation, we need to add a value since we have zeroes
+# Also, the log transformation will give negative values, which some ordination tests don't use. Potentially use the absolute value after the transformation since we don't have a mix of positive and negative values, so I think it could be done.
+# However, taking the absolute values will turn over the data. For 
+# With the clean data, it's ready for ordination or clustering analysis
+voc_clean[,4:78] %>% 
+  mutate_all(., list( ~ ifelse(. == 0, 1, .))) %>% 
+  min()
 
-#' Any negative values are zero
-#' Keep the maximum value for each sample
-makezero <- function(x) ifelse(x < 0, 0, x)
-
-voc_raw %>% 
-  mutate_if(is.numeric, makezero) %>% 
-  arrange(sampid, .by_group = TRUE)-> voc_data
-  
-#' Questions here regarding the next steps
-#' should we first select the max values for specific voc?
-#' or should we actually create these 10, 20% cutoffs first?
-#' IMO we should select the max for each sample, so each sample has only one value and then we can select 10 or 20% representation. If not, some samples would get extra?
-#' 
-
-
-# This function works to remove the volatiles that are not found in a given percentage threshold for observations
-voc_filter <- function(voc_data, threshold = NULL){
-  voc_data %>% 
-    select(-famid, -sampid, -ssex) %>% 
-    mutate_if(is.numeric, function(x) ifelse(x > 0, 1, 0)) %>% 
-    colSums() %>% 
-    data.frame() %>% 
-    rownames_to_column() %>%
-    setNames(., c("voc", "count")) %>% 
-    mutate(prcnt = count/nrow(voc_data)) %>% 
-    filter(prcnt >= threshold) -> voc_filter
-  
-  voc_data %>% 
-    select(famid, sampid, ssex, c(voc_filter$voc))
-  
-}
-
-test <- voc_filter(voc_data, 0.9)
-
-# The function works, now we need to keep only one sample, remove duplicates by taking only the max. 
-# Following Leslie's code,do the NMDS, which doesn't converge, but use sex instead of family
-voc_clean <- voc_filter(voc_data, 0.1) %>% 
-  group_by(sampid) %>%
-  mutate_at(vars(starts_with("m")), max) %>% 
-  ungroup () %>% 
-  distinct() %>% 
-  mutate(ssex = str_to_lower(as.character(ssex)))
+voc_ordination <- abs(log10(voc_clean[4:78] + 1e-26)) + 26
 
 
 nmd_output <- metaMDS(voc_clean[,4:78])
@@ -136,7 +85,7 @@ corrplot(voc_corr, method = "circle", type = "upper", order = "hclust", tl.col =
 # 
 # PCA attempt
 voc_clean %>% 
-  filter(ssex == "f") %>% 
+  filter(ssex == "m") %>% 
   select(-famid, -ssex, -sampid) -> sub_voc
 
 sub_voc %>% 
@@ -146,8 +95,8 @@ sub_voc %>%
   mutate_all(., list( ~ ifelse(. == 0, 1, .))) %>% 
   min()
 
-pca_voc <- abs(log10(sub_voc + 1e-26))
-PCA <- vegan::rda(pca_voc, scale = TRUE)
+pca_voc <- abs(log10(voc_clean[,4:78] + 1e-26))
+PCA <- vegan::rda(pca_voc, scale = FALSE)
 PCA
 #plot(PCA)
 
@@ -163,3 +112,4 @@ plot(dat_dca, display = "species")
 
 dat_nmds <- metaMDS(pca_voc)
 plot(dat_nmds)
+stressplot(dat_nmds)
